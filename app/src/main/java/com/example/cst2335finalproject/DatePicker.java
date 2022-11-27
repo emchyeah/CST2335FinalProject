c776af934e0878fe7565c1e70647f3a4fa1688b1
Daniel Longo <long0266@algonquinlive.com>
2022-11-27 17:17:33 -0500

    created navigation drawer, toolbar, main activity, and datepicker activity

--- app/src/main/java/com/example/cst2335finalproject/DatePicker.java
+++ app/src/main/java/com/example/cst2335finalproject/DatePicker.java
@@ -1,13 +1,15 @@
 package com.example.cst2335finalproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends BaseActivity {
+public class DatePicker extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_datepicker);
         Load();
     }
 }
