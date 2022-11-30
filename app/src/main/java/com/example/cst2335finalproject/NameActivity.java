89654f29382be248154ba5d1ac526e4952478dc4
meigan <chea0024@algonquinlive.com>
2022-11-30 16:53:49 -0500

    created intent to move to, ideally, space page

--- app/src/main/java/com/example/cst2335finalproject/NameActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/NameActivity.java
@@ -11,6 +11,7 @@ public class NameActivity extends AppCompatActivity {
 
     TextView nameText;
     Button changeName;
+    Button pix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +19,29 @@ public class NameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_name);
 
         nameText = findViewById(R.id.name);
-
         String newTextView = nameText.getText().toString();
 
         Intent dataSent = getIntent();
         //getting data from previous page
         String dataName = dataSent.getStringExtra("name");
 
+        //sets the TextView to show the name set from MainActivity to this activity
         nameText.setText(newTextView + " " + dataName + "!");
 
         changeName = findViewById(R.id.changeName);
+        //button click goes to previous activity
         changeName.setOnClickListener(click -> {
             setResult(0, dataSent);
-            //goes back to previous activity
             finish();
         });
+
+        //creates the intent to go to next activity
+        Intent intent = new Intent(this, NameActivity.class);
+                //TODO need to make next activity to replace NameActivity.class
+        pix = findViewById(R.id.Space);
+        //button click goes to the next activity
+        pix.setOnClickListener(click -> startActivity(intent));
+
 
 
     }
